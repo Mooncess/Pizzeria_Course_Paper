6c0af06ea7b43530aa392c8678a232f360070f22
Mooncess <shulik.2003@mail.ru>
2023-11-17 22:11:43 +0300

    Endpoints for size, order status are implemented

--- src/main/java/ru/mooncess/pizzeriacoursepaper/controllers/AdminController.java
+++ src/main/java/ru/mooncess/pizzeriacoursepaper/controllers/AdminController.java
@@ -4,8 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.mooncess.pizzeriacoursepaper.dto.AdditiveCreateDto;
-import ru.mooncess.pizzeriacoursepaper.entities.Additive;
-import ru.mooncess.pizzeriacoursepaper.entities.Dough;
+import ru.mooncess.pizzeriacoursepaper.entities.*;
 import ru.mooncess.pizzeriacoursepaper.service.AdminService;
 
 import java.util.List;
@@ -16,7 +15,7 @@ import java.util.List;
 public class AdminController {
     private final AdminService adminService;
 
-    // Additive's endpoints
+    // Additive endpoints
     @GetMapping("/additive")
     public ResponseEntity<List<Additive>> getAllAdditives() {
         return adminService.getAllAdditives();
@@ -42,15 +41,10 @@ public class AdminController {
         return adminService.deleteAdditive(id);
     }
 
-    // Dough's endpoints
+    // Dough endpoints
     @GetMapping("/dough")
     public ResponseEntity<List<Dough>> getAllDough() {
         return adminService.getAllDough();
-    }
-
-    @GetMapping("/dough/{id}")
-    public ResponseEntity<?> getDoughById(@PathVariable Integer id) {
-        return adminService.getDoughById(id);
     }
 
     @PostMapping("/dough")
@@ -66,5 +60,47 @@ public class AdminController {
     @DeleteMapping("/dough/{id}")
     public ResponseEntity<Void> deleteDough(@PathVariable Integer id) {
         return adminService.deleteDough(id);
+    }
+
+    // Size endpoints
+    @GetMapping("/size")
+    public ResponseEntity<List<Size>> getAllSize() {
+        return adminService.getAllSize();
+    }
+
+    @PostMapping("/size")
+    public ResponseEntity<Size> createSize(@RequestParam String name) {
+        return adminService.createSize(name);
+    }
+
+    @PutMapping("/size/{id}")
+    public ResponseEntity<Size> updateSize(@PathVariable Integer id, @RequestParam String name) {
+        return adminService.updateSize(id, name);
+    }
+
+    @DeleteMapping("/size/{id}")
+    public ResponseEntity<Void> deleteSize(@PathVariable Integer id) {
+        return adminService.deleteSize(id);
+    }
+
+    // Order status endpoints
+    @GetMapping("/order-status")
+    public ResponseEntity<List<OrderStatus>> getAllOrderStatus() {
+        return adminService.getAllOrderStatus();
+    }
+
+    @PostMapping("/order-status")
+    public ResponseEntity<OrderStatus> createOrderStatus(@RequestParam String name) {
+        return adminService.createOrderStatus(name);
+    }
+
+    @PutMapping("/order-status/{id}")
+    public ResponseEntity<OrderStatus> updateOrderStatus(@PathVariable Integer id, @RequestParam String name) {
+        return adminService.updateOrderStatus(id, name);
+    }
+
+    @DeleteMapping("/order-status/{id}")
+    public ResponseEntity<Void> deleteOrderStatus(@PathVariable Integer id) {
+        return adminService.deleteOrderStatus(id);
     }
 }
--- src/main/java/ru/mooncess/pizzeriacoursepaper/service/AdminService.java
+++ src/main/java/ru/mooncess/pizzeriacoursepaper/service/AdminService.java
@@ -4,14 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import ru.mooncess.pizzeriacoursepaper.dto.AdditiveCreateDto;
 import ru.mooncess.pizzeriacoursepaper.entities.Additive;
 import ru.mooncess.pizzeriacoursepaper.entities.Dough;
+import ru.mooncess.pizzeriacoursepaper.entities.OrderStatus;
+import ru.mooncess.pizzeriacoursepaper.entities.Size;
 import ru.mooncess.pizzeriacoursepaper.mappers.AdditiveMapper;
 import ru.mooncess.pizzeriacoursepaper.repositories.AdditiveRepository;
 import ru.mooncess.pizzeriacoursepaper.repositories.DoughRepository;
+import ru.mooncess.pizzeriacoursepaper.repositories.OrderStatusRepository;
+import ru.mooncess.pizzeriacoursepaper.repositories.SizeRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,11 +25,15 @@ public class AdminService {
     private final AdditiveMapper additiveMapper;
 
     private final DoughRepository doughRepository;
+
+    public final SizeRepository sizeRepository;
+
+    private final OrderStatusRepository orderStatusRepository;
+
     public ResponseEntity<List<Additive>> getAllAdditives() {
         List<Additive> additives = additiveRepository.findAll();
         return ResponseEntity.ok(additives);
     }
-
     public ResponseEntity<?> getAdditiveById(Long id) {
         Optional<Additive> optionalAdditive = additiveRepository.findById(id);
         if (optionalAdditive.isPresent()) {
@@ -37,13 +43,11 @@ public class AdminService {
             return ResponseEntity.notFound().build();
         }
     }
-
     public ResponseEntity<Additive> createAdditive(AdditiveCreateDto additive) {
         Additive newAdditive = additiveMapper.toEntity(additive);
         Additive createdAdditive = additiveRepository.save(newAdditive);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdAdditive);
     }
-
     public ResponseEntity<Additive> updateAdditive(Long id, AdditiveCreateDto additive) {
         Optional<Additive> optionalAdditive = additiveRepository.findById(id);
         if (optionalAdditive.isPresent()) {
@@ -55,7 +59,6 @@ public class AdminService {
             return ResponseEntity.notFound().build();
         }
     }
-
     public ResponseEntity<Void> deleteAdditive(Long id) {
         Optional<Additive> optionalAdditive = additiveRepository.findById(id);
         if (optionalAdditive.isPresent()) {
@@ -65,29 +68,16 @@ public class AdminService {
             return ResponseEntity.notFound().build();
         }
     }
-
     public ResponseEntity<List<Dough>> getAllDough() {
         List<Dough> doughs = doughRepository.findAll();
         return ResponseEntity.ok(doughs);
     }
-
-    public ResponseEntity<?> getDoughById(Integer id) {
-        Optional<Dough> optionalDough = doughRepository.findById(id);
-        if (optionalDough.isPresent()) {
-            Dough dough = optionalDough.get();
-            return ResponseEntity.ok(dough);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
     public ResponseEntity<Dough> createDough(String name) {
         Dough dough = new Dough();
         dough.setName(name);
         Dough createdDough = doughRepository.save(dough);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdDough);
     }
-
     public ResponseEntity<Dough> updateDough(Integer id, String name) {
         Optional<Dough> optionalDough = doughRepository.findById(id);
         if (optionalDough.isPresent()) {
@@ -99,11 +89,79 @@ public class AdminService {
             return ResponseEntity.notFound().build();
         }
     }
-
     public ResponseEntity<Void> deleteDough(Integer id) {
         Optional<Dough> optionalDough = doughRepository.findById(id);
         if (optionalDough.isPresent()) {
             doughRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    public ResponseEntity<List<Size>> getAllSize() {
+        List<Size> sizes = sizeRepository.findAll();
+        return ResponseEntity.ok(sizes);
+    }
+    public ResponseEntity<Size> createSize(String name) {
+        Size size = new Size();
+        size.setName(name);
+        Size createdSize = sizeRepository.save(size);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdSize);
+    }
+    public ResponseEntity<Size> updateSize(Integer id, String name) {
+        Optional<Size> optionalSize = sizeRepository.findById(id);
+        if (optionalSize.isPresent()) {
+            Size updatedSize = sizeRepository.getById(id);
+            updatedSize.setName(name);
+            Size savedSize = sizeRepository.save(updatedSize);
+            return ResponseEntity.ok(savedSize);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    public ResponseEntity<Void> deleteSize(Integer id) {
+        Optional<Size> optionalSize = sizeRepository.findById(id);
+        if (optionalSize.isPresent()) {
+            sizeRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    public ResponseEntity<List<OrderStatus>> getAllOrderStatus() {
+        List<OrderStatus> orderStatuses = orderStatusRepository.findAll();
+        return ResponseEntity.ok(orderStatuses);
+    }
+    public ResponseEntity<?> getOrderStatusById(Integer id) {
+        Optional<OrderStatus> optionalOrderStatus = orderStatusRepository.findById(id);
+        if (optionalOrderStatus.isPresent()) {
+            OrderStatus orderStatus = optionalOrderStatus.get();
+            return ResponseEntity.ok(orderStatus);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    public ResponseEntity<OrderStatus> createOrderStatus(String name) {
+        OrderStatus orderStatus = new OrderStatus();
+        orderStatus.setName(name);
+        OrderStatus createdOrderStatus = orderStatusRepository.save(orderStatus);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdOrderStatus);
+    }
+    public ResponseEntity<OrderStatus> updateOrderStatus(Integer id, String name) {
+        Optional<OrderStatus> optionalOrderStatus = orderStatusRepository.findById(id);
+        if (optionalOrderStatus.isPresent()) {
+            OrderStatus updatedOrderStatus = orderStatusRepository.getById(id);
+            updatedOrderStatus.setName(name);
+            OrderStatus savedOrderStatus = orderStatusRepository.save(updatedOrderStatus);
+            return ResponseEntity.ok(savedOrderStatus);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    public ResponseEntity<Void> deleteOrderStatus(Integer id) {
+        Optional<OrderStatus> optionalOrderStatus = orderStatusRepository.findById(id);
+        if (optionalOrderStatus.isPresent()) {
+            orderStatusRepository.deleteById(id);
             return ResponseEntity.noContent().build();
         } else {
             return ResponseEntity.notFound().build();
